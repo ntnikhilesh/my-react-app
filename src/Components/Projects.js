a86699c3f89adf724d5c8bb8eccc4dc130f94fc8
ntnikhilesh <ntnikhilesh@gmail.com>
2018-01-16 18:44:30 +0530

    New component

---
+++ src/Components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        Hello CC Projects...
+      </div>
+    );
+  }
+}
+
+export default Projects;
